3c6aeeef8df2573c351985f9691006a172dcb324
Hernan Cote <hernan.cote@zemoga.com>
2021-01-06 17:30:38 -0500

    add meta keywords

--- src/components/Meta/Meta.jsx
+++ src/components/Meta/Meta.jsx
@@ -4,7 +4,7 @@ import { Helmet } from 'react-helmet';
 const Meta = ({
   title = 'Hernán Cote',
   description = 'Hernán Cote\'s Portfolio - Software Craftsman',
-  keywords = 'Programming, Portfolio, C#, ASP.NET, JavaScript, React, Work, Development, Web Development, Software Architecture',
+  keywords = 'Programming, Portfolio, C#, ASP.NET, JavaScript, React, Work, Development, Web Development, Software Architecture, Hernán Cote, Hernan Cote, Hernan Felipe Cote, Hernan Cote Martinez, Hernán Felipe Cote Martínez',
   author = 'Hernán Cote',
 }) => (
   <Helmet>
